3bd74dca01475b1f20e272d765ebb141d830885e
mochi <mochikichi@live.jp>
2021-06-25 11:22:00 +0900

    クリティカルとミスを追加

--- quest.rb
+++ quest.rb
@@ -1,3 +1,7 @@
+Critical_Coefficient = 1.5
+Critical_Probability = 5
+Miss_Probability = 10
+
 class Mob
   attr_reader :name, :ap
   attr_accessor :hp
@@ -9,7 +13,31 @@ class Mob
   end
 
   def attack(target)
-    target.hp -= ap
+    puts "#{name}の攻撃！"
+    return puts 'ミス！！！' if miss?
+    if critical?
+      puts 'クリティカルーーー！！！'
+      critical_attack(target)
+      damage = (ap * Critical_Coefficient).round
+    else
+      target.hp -= ap
+      damage = ap
+    end
+    puts "#{target.name}に#{damage}のダメージ！"
+  end
+
+  private
+
+  def critical?
+    rand(Critical_Probability) == 1
+  end
+
+  def miss?
+    rand(Miss_Probability) == 1
+  end
+
+  def critical_attack(target)
+    target.hp -= (ap * Critical_Coefficient).round
   end
 end
 
@@ -23,19 +51,17 @@ puts '================='
 
 until (satan.hp <= 0 || hero.hp <= 0)
   hero.attack(satan)
-  puts "#{hero.name}の攻撃！　#{satan.name}に#{hero.ap}のダメージ！"
   if satan.hp <= 0
     break puts "#{satan.name}を倒した！世界に平和が訪れた。"
   else
-    puts "#{hero.name} HP:#{hero.hp}, 攻撃力:#{hero.ap}"
+    puts "#{satan.name} HP:#{satan.hp}, 攻撃力:#{satan.ap}"
   end
 
   satan.attack(hero)
-  puts "#{satan.name}の攻撃！　#{hero.name}に#{satan.ap}のダメージ！"
   if hero.hp <= 0
     break puts '勇者は倒れた・・・・・。'
   else
-    puts "#{satan.name} HP:#{satan.hp}, 攻撃力:#{satan.ap}"
+    puts "#{hero.name} HP:#{hero.hp}, 攻撃力:#{hero.ap}"
   end
   puts '================='
 end
