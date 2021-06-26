f908a59c8dc69249c0b20afa57c33847738d8514
mochi <mochikichi@live.jp>
2021-06-26 12:21:49 +0900

    攻撃を物理と魔法に分ける

--- quest.rb
+++ quest.rb
@@ -1,52 +1,66 @@
-Critical_Coefficient = 1.5
-Critical_Probability = 5
-Miss_Probability = 10
+CRITICAL_COEFFICIENT = 1.5
+CRITICAL_PROBABILITY = 5
+MISS_PROBABILITY = 10
+MAGIC_ATTACK_PROBABILITY = 3
 
 class Mob
-  attr_reader :name, :ap
+  attr_reader :name, :atk, :mat
   attr_accessor :hp
-  def initialize(name:, max_hp:, ap:)
+  def initialize(name:, max_hp:, atk:, mat:)
     @name = name
     @max_hp = max_hp
-    @ap = ap
     @hp = @max_hp
+    @atk = atk
+    @mat = mat
   end
 
   def attack(target)
     puts "#{name}の攻撃！"
     return puts 'ミス！！！' if miss?
-    if critical?
+    if magic_attack?
+      puts '★魔法攻撃！'
+      target.hp -= mat
+      damage = mat
+    elsif critical?
       puts 'クリティカルーーー！！！'
       critical_attack(target)
-      damage = (ap * Critical_Coefficient).round
+      damage = (atk * CRITICAL_COEFFICIENT).round
     else
-      target.hp -= ap
-      damage = ap
+      target.hp -= atk
+      damage = atk
     end
     puts "#{target.name}に#{damage}のダメージ！"
+  end
+
+  def show_status
+    "#{name} HP:#{hp}, 攻撃力:#{atk}, 魔法攻撃力:#{mat}"
   end
 
   private
 
   def critical?
-    rand(Critical_Probability) == 1
+    rand(CRITICAL_PROBABILITY) == 1
   end
 
   def miss?
-    rand(Miss_Probability) == 1
+    rand(MISS_PROBABILITY) == 1
+  end
+
+  def magic_attack?
+    rand(MAGIC_ATTACK_PROBABILITY) == 1
   end
 
   def critical_attack(target)
-    target.hp -= (ap * Critical_Coefficient).round
+    target.hp -= (atk * CRITICAL_COEFFICIENT).round
   end
 end
 
-hero = Mob.new(name: '勇者', max_hp: 350, ap: 70)
-satan = Mob.new(name: '魔王', max_hp: 400, ap: 60)
+hero = Mob.new(name: '勇者', max_hp: 350, atk: 70, mat: 40)
+satan = Mob.new(name: '魔王', max_hp: 400, atk: 60, mat: 70)
 
 puts 'バトル開始！'
-puts "#{hero.name} HP:#{hero.hp}, 攻撃力:#{hero.ap}"
-puts "#{satan.name} HP:#{satan.hp}, 攻撃力:#{satan.ap}"
+puts hero.show_status
+puts satan.show_status
 puts '================='
 
 until (satan.hp <= 0 || hero.hp <= 0)
@@ -54,14 +68,14 @@ until (satan.hp <= 0 || hero.hp <= 0)
   if satan.hp <= 0
     break puts "#{satan.name}を倒した！世界に平和が訪れた。"
   else
-    puts "#{satan.name} HP:#{satan.hp}, 攻撃力:#{satan.ap}"
+    puts satan.show_status
   end
 
   satan.attack(hero)
   if hero.hp <= 0
     break puts '勇者は倒れた・・・・・。'
   else
-    puts "#{hero.name} HP:#{hero.hp}, 攻撃力:#{hero.ap}"
+    puts hero.show_status
   end
   puts '================='
 end
